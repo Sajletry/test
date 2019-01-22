b913496ecdb7865ed7ba8fb488571a288e9190b9
Sajletry <vorochaevv@gmail.com>
2019-01-22 21:29:56 +0300

    Adding logs into TodoView

--- src/views/todoView.js
+++ src/views/todoView.js
@@ -1,8 +1,8 @@
 export class ToDoView {
-
     getView(todo){
         let container = document.createElement('div');
         container.innerText = todo.name;
+        console.log(todo.name);
         document.body.append(container);
     }
 }
